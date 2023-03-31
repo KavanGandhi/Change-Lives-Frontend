843c8878cc0c7e9381ce74ffb5079a68187c9bf8
KavanGandhi <kavangandhi1@gmail.com>
2023-03-31 21:52:39 +0530

    change lives fronrend part added

---
+++ index.js
@@ -0,0 +1,22 @@
+// window.addEventListener('scroll', function(){
+// const navbar = document.querySelector('.navbar');
+// let top = navbar.offsetTop;
+//   if (window.scrollY >= top) {    
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');    
+//   }
+
+// });
+
+function toggle(){
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("active");
+}
+
+
+
+
+
